c1a85a3c563c11c230123d802add8103c4cae6a5
JunSik Jeong <jjs9915@gmail.com>
2021-12-20 22:08:50 +0900

    키보드 입력을 통한 구구단 출력

--- ICanDo/main.cpp
+++ ICanDo/main.cpp
@@ -4,27 +4,12 @@ using namespace std;
 
 int main()
 {
-	int i = 0;
-	int j = 0;
-	int result = 0;
-
-	for (i = 2; i < 10; i++)
-	{
-		cout << " " << i << "´Ü" << '\t';
-	}
-
-	cout << endl;
-
-	for (i = 1; i < 10; i++)
-	{
-		for (j = 2; j < 10; j++)
-		{
-			result = j * i;
-			cout << j << "x" << i << "=" << result << '\t';
-		}
-
-		cout << endl;
-	}
+    int n;
+    cin >> n;
+    for (int i = 1; i <= 9; i++)
+    {
+        cout << n << " * " << i << " = " << n * i << '\n';
+    }
 
 	return 0;
 }
