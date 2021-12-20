1bd3e5c21fdd0a335de6ebbe343d250709af48b0
JunSik Jeong <jjs9915@gmail.com>
2021-12-20 22:10:59 +0900

    change variable name : n to in

--- ICanDo/main.cpp
+++ ICanDo/main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int in;
+    cin >> in;
     for (int i = 1; i <= 9; i++)
     {
-        cout << n << " * " << i << " = " << n * i << endl;
+        cout << in << " * " << i << " = " << in * i << endl;
     }
 
 	return 0;
