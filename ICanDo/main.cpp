960c021edc793662e476e52b7f1f943f23cbb09b
JunSik Jeong <jjs9915@gmail.com>
2021-12-20 21:57:58 +0900

    구구단 가로로 출력

--- ICanDo/main.cpp
+++ ICanDo/main.cpp
@@ -4,9 +4,22 @@ using namespace std;
 
 int main()
 {
-	string title = "This is GitHub Test Project";
+	int i = 0;
+	int j = 0;
+	int result = 0;
 
-	cout << title;
+	for (i = 2; i < 10; i++)
+	{
+		cout << i << "´Ü" << '\t';
 
+		for (j = 1; j < 10; j++)
+		{
+			result = i * j;
+
+			cout << i << "x" << j << "=" << result << '\t';
+		}
+
+		cout << endl;
+	}
 	return 0;
 }
