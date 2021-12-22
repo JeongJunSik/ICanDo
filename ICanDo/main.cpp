f3470a2ec222486ce8a60283a773765e5365c433
Durumyisking <cww9512@sungkyul.ac.kr>
2021-12-22 12:16:09 +0900

    commit

--- ICanDo/main.cpp
+++ ICanDo/main.cpp
@@ -14,8 +14,15 @@ void table3()
 {
     for (int i = 1; i <= 9; i++)
     {
-        cout << "2 x " << i << " = " << 2 * i << endl;
+        cout << "4 x " << i << " = " << 3 * i << endl;
     }
+}
+void table4()
+{
+	for (int i = 1; i <= 9; i++)
+	{
+		cout << "3 x " << i << " = " << 4 * i << endl;
+	}
 }
 
 int main()
@@ -24,6 +31,9 @@ int main()
     cout << "-----------" << endl;
 
     table3();
+	cout << "-----------" << endl;
+
+	table4();
 
 	return 0;
 }
